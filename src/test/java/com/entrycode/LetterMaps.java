24739ca83d508892b2c7e3f9a06bad8e77c75549
Shakthi Ganeshan <100307146+Barry2211@users.noreply.github.com>
2025-07-21 11:53:48 +0530

    enhanced

--- src/test/java/com/entrycode/LetterMaps.java
+++ src/test/java/com/entrycode/LetterMaps.java
@@ -3,20 +3,22 @@ package com.entrycode;
 import java.util.*;
 
 public class LetterMaps {
-
-	public static void main(String[] args) {
-		String candy = "lollipop";
+	
+	public static void checker(String spring) {
 		Map<String,Integer> letters = new HashMap<>();
 		letters.put("l", 1);
 		letters.put("o", 2);
 		letters.put("i", 3);
 		letters.put("p", 4);
-		for(int i=0;i<candy.length();i++) {
-			String cha=candy.charAt(i)+"";
+		for(int i=0;i<spring.length();i++) {
+			String cha=spring.charAt(i)+"";
 			System.out.println(letters.get(cha.toLowerCase()));
 		}
-		
+	}
 
+	public static void main(String[] args) {
+		String candy = "lollipop";
+		checker(candy);
 	}
 
 }
